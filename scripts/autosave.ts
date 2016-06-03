1c74eafd370631ae6a86519074a4c25a6a3f2d2b
Sean Ferguson <sferg@microsoft.com>
2016-06-02 17:01:04 -0700

    Simplified the extension to not be configurable

---
+++ scripts/autosave.ts
@@ -0,0 +1,45 @@
+import VSS_Utils_Core = require("VSS/Utils/Core");
+import Q = require("q");
+import {IWorkItemFieldChangedArgs, IWorkItemNotificationListener} from "TFS/WorkItemTracking/ExtensionContracts"
+import {IWorkItemFormService, WorkItemFormService} from "TFS/WorkItemTracking/Services";
+
+export class Autosave {
+    private _throttledFieldChangeDelegate: IArgsFunctionR<any>;
+    private _workItemFormService: IWorkItemFormService;
+
+    public initialize(): IPromise<void> {
+        var deferred = Q.defer<void>();
+
+        WorkItemFormService.getService().then(
+            (workItemFormService: IWorkItemFormService) => {
+                this._workItemFormService = workItemFormService;
+
+                this._throttledFieldChangeDelegate = VSS_Utils_Core.throttledDelegate(
+                    this,
+                    2500,
+                    (args: IWorkItemFieldChangedArgs) => {
+                        this._onFieldChanged(args);
+                    });
+                deferred.resolve(null);
+            });
+
+        return deferred.promise;
+    }
+
+    public register(): void {
+        VSS.register("autosave", {
+            onFieldChanged: (args: IWorkItemFieldChangedArgs) => {
+                this._throttledFieldChangeDelegate(args);
+            }
+        } as IWorkItemNotificationListener);
+    }
+
+    private _onFieldChanged(args: IWorkItemFieldChangedArgs): void {
+        // Quick check, is this a new work item.  If so, do not autosave
+        this._workItemFormService.isNew().then((isNew: boolean) => {
+            if (!isNew) {
+                this._workItemFormService.beginSaveWorkItem(() => { }, () => { });
+            }
+        });
+    }
+}
